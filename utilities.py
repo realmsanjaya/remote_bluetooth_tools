efb1ce533633ccfab32e0261a25260ff522c53f8
msanjaya <msanjaya@stetson.edu>
2021-04-09 21:04:39 -0400

    created faker utility

--- utilities.py
+++ utilities.py
@@ -1,4 +1,5 @@
 import bluetooth
+from faker import Faker
 
 # Shows us bluetooth devices nearby
 def find_bluetooth():
@@ -7,4 +8,16 @@ def find_bluetooth():
     print(devices)
 
 
-find_bluetooth()
+# find_bluetooth()
+
+
+# Generate Random Bluetooth Devices
+def gen_devices():
+    """ This function generates the desired number of fake devices """
+    fake_data = Faker()
+    DEVICE_TYPE = ['BS', 'PH', 'HS', 'IOT']
+    device_name = fake_data.android_platform_token()
+    device_type = 'BS'
+    device_mac = fake_data.mac_address()
+    device = {'device_name': device_name, 'device_type': device_type, 'device_mac': device_mac}
+    return device
