c66b7f361a51b181f7e2b29731099737d9ea720a
msanjaya <msanjaya@stetson.edu>
2021-04-09 21:49:59 -0400

    adding updates to client

--- client.py
+++ client.py
@@ -1,24 +1,41 @@
 import json
-import bluetooth
+import requests
+from utilities import gen_devices
 
-class Client:
-    def __init__(self, device_name):
-        self.device_name = device_name
+def test_server():
+    """ This function is used to simulate a client  """
+    # Set constants
+    URL = "http://127.0.0.1:8000/api/bluetoothdevices/"
+    DEVICE = gen_devices()
+    requests.post(URL, data = DEVICE)
+    return f"Success"
 
-    def start_scan(self):
-        """ Used to start a scan for bluetooth device """
-        # enable bluetooth
-        # scan for devices
-        # return device information in a json format
-        devices = bluetooth.discover_devices(lookup_names=True)
-        for addr, name in devices:
-            print(f"Address: {addr}, Name: {name}")
-        return f'make something cool {self.device_name}'
+if __name__ == '__main__':
+    test_server()
 
-# Data Structure
-data = {'device_id': 'raspberrypi', 'devices': [('E0:D1:E6:04:F4:F9', 'MINIJAMBOX by Jawbone')] }
 
-# Run program
-client1 = Client(device_name="raspberrypi")
-client1.start_scan()
+# """
+# import bluetooth
 
+# class Client:
+#     def __init__(self, device_name):
+#         self.device_name = device_name
+
+#     def start_scan(self):
+#         """ Used to start a scan for bluetooth device """
+#         # enable bluetooth
+#         # scan for devices
+#         # return device information in a json format
+#         devices = bluetooth.discover_devices(lookup_names=True)
+#         for addr, name in devices:
+#             print(f"Address: {addr}, Name: {name}")
+#         return f'make something cool {self.device_name}'
+
+# # Data Structure
+# data = {'device_id': 'raspberrypi', 'devices': [('E0:D1:E6:04:F4:F9', 'MINIJAMBOX by Jawbone')] }
+
+# # Run program
+# client1 = Client(device_name="raspberrypi")
+# client1.start_scan()
+
+# """
