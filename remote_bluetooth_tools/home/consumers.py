9f300acb493439587dc337f12b5c2a8f2c56a6e7
msanjaya <msanjaya@stetson.edu>
2021-04-17 00:58:02 -0400

    updating websockets

--- remote_bluetooth_tools/home/consumers.py
+++ remote_bluetooth_tools/home/consumers.py
@@ -1,7 +1,9 @@
 import json
 from random import randint
 from time import sleep
+from random import choice
 from channels.generic.websocket import WebsocketConsumer
+from .models import BluetoothDevice
 
 class WSConsumer(WebsocketConsumer):
     def connect(self):
@@ -9,5 +11,20 @@ class WSConsumer(WebsocketConsumer):
 
         for i in range(1000):
             message = {'message': randint(1,100)}
+            self.send(json.dumps(message))
+            sleep(1)
+
+
+class DeviceConsumer(WebsocketConsumer):
+    """ This consumer will be used to stream devices from database """
+    
+    
+    def connect(self):
+        self.accept()
+        devices = [device.device_name for device in BluetoothDevice.objects.all()]
+
+        for i in range(1000):
+            device = choice(devices)
+            message = {'device': device, 'message': 'You connected' }
             self.send(json.dumps(message))
             sleep(1)
--- remote_bluetooth_tools/home/routing.py
+++ remote_bluetooth_tools/home/routing.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .consumers import WSConsumer
+from .consumers import WSConsumer, DeviceConsumer
 
 
 ws_urlpatterns = [
-    path('ws/app/', WSConsumer.as_asgi())
+    path('ws/app/', WSConsumer.as_asgi()),
+    path('ws/devices/', DeviceConsumer.as_asgi())
 ]
